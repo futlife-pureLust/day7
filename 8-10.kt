9c5c1696aec3bacf5dc0ed84d2a0d6bef9d12fd1
futlife-pureLust <futlife.love.daimon@gmail.com>
2021-06-24 16:08:20 +0800

    8-10

--- 8-10.kt
+++ 8-10.kt
@@ -1,16 +1,33 @@
-const val TAVERN_NAME = "Taernyl's Folly"
+import kotlin.math.roundToInt
 
-fun main(args: Array<String>) {
-    placeOrder("shandy,Dragon's Breath, 5.91")
+const val  TAVERN_NAME = "Taerny's Folly"
+
+var playerGold = 0
+var playerSilver = 0
+var playerDrogonCoin = 5
+var totalPurse = playerDrogonCoin * 1.43
+
+fun main() {
+    placeOrder("shandy ,Dragon's Breath, 5.91")
 }
-
-fun performPurchase() {
+fun performPurchase(price: Double) {
     displayBalance()
+    println("玩家錢包總餘額: ${"%.2f".format(totalPurse)} 個金幣")
+    if (totalPurse >= price) {
+        println("購買品項需要: $price 個金幣")
+        totalPurse = (totalPurse - price)/1.43
+        playerGold = playerGold - "%.0f".format(price).toInt()
+        playerSilver = (totalPurse % 1 * 100).roundToInt()
+        println("玩家購買後錢包餘額: dragon: ${"%.4f".format(totalPurse)}")
+    } else {
+        println("抱歉，錢包裡面錢不夠了！！！")
+    }
+}
+private  fun displayBalance() {
+    println("玩家錢包餘額: dragan: $playerDrogonCoin")
 }
 
-private fun displayBalance() {
-    println("Player's purse balance: Gold: $playerGold , Silver: $playerSilver")
-}
+
 
 private fun toDragonSpeak(phrase: String) =
     phrase.replace(Regex("[aeiouAEIOU]")){
@@ -33,15 +50,13 @@ private  fun placeOrder(menuData: String){
     val indexOfAppstrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfAppstrophe)
     println("Madrigal對 $tavernMaster 說了他們訂的酒水。 ")
-
     val (type,name,price) = menuData.split(',')
     val message = "Madrigal 買了一杯 $name ($type) 花了 $price."
     println(message)
-
-    performPurchase()
+    performPurchase(price.toDouble())
 
     val phrase = if (name == "Dragon's Breath"){
-        "Madrigal 喝下後喊出: ${toDragonSpeak("IT'S GOT WHAT ADVENTURERS CRAVE!")}"
+        "Madrigal 驚呼道: ${toDragonSpeak("Ah, dlicious $name!")}"
     } else {
         "Madrigal 說道: 感謝 $name ."
     }
